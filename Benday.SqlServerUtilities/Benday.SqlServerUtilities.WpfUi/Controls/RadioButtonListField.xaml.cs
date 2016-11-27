b25d317952bf704cf318244b8508b9c033039486
Benjamin Day <benday@live.com>
2016-11-27 09:48:07 -0500

    Basic layout for database search user control without bindings.

---
+++ Benday.SqlServerUtilities/Benday.SqlServerUtilities.WpfUi/Controls/RadioButtonListField.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Benday.SqlServerUtilities.WpfUi.Controls
+{
+    /// <summary>
+    /// Interaction logic for RadioButtonListField.xaml
+    /// </summary>
+    public partial class RadioButtonListField : UserControl
+    {
+        public RadioButtonListField()
+        {
+            InitializeComponent();
+        }
+
+        public string LabelText
+        {
+            get
+            {
+                return (string)this.GetValue(LabelTextProperty);
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.SetValue(LabelTextProperty, String.Empty);
+                    m_label.Text = String.Empty;
+                }
+                else
+                {
+                    this.SetValue(LabelTextProperty, value);
+                }
+            }
+        }
+
+        public static readonly DependencyProperty LabelTextProperty = DependencyProperty.Register(
+          "LabelText", typeof(string), typeof(RadioButtonListField), new PropertyMetadata(String.Empty, LabelTextPropertyChanged));
+
+        private static void LabelTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TextboxField target = d as TextboxField;
+
+            var tempValue = e.NewValue as string;
+
+            if (target != null && tempValue != null)
+            {
+                string newValueToUpper = tempValue.ToUpper();
+
+                target.m_label.Text = newValueToUpper;
+            }
+        }
+
+    }
+}
