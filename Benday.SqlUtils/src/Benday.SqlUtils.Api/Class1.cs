7f6343fa17aea5a224ef179e155fbcb800a545a8
Benjamin Day <benday@benday.com>
2020-05-24 08:35:59 -0400

    Added new solution structure.

---
+++ Benday.SqlUtils/src/Benday.SqlUtils.Api/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Benday.SqlUtils.Api
+{
+    public class Class1
+    {
+    }
+}
---
+++ Benday.SqlUtils/src/Benday.SqlUtils.ConsoleUi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Benday.SqlUtils.ConsoleUi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
