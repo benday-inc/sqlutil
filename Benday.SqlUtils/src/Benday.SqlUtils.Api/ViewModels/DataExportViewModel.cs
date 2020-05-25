862c1c5f1f8471cd2127fd6fb25cba677e833e2a
Benjamin Day <benday@benday.com>
2020-05-25 10:41:40 -0400

    Started implementing data export functionality.

--- Benday.SqlUtils/src/Benday.SqlUtils.Api/ViewModels/DataExportViewModel.cs
+++ Benday.SqlUtils/src/Benday.SqlUtils.Api/ViewModels/DataExportViewModel.cs
@@ -1,11 +1,151 @@
 ﻿using Benday.Presentation;
+using Benday.SqlUtils.Core;
+using Benday.SqlUtils.Core.ViewModels;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Benday.SqlUtils.Api.ViewModels
 {
-    public class DataExportViewModel : ViewModelBase
+    public class DataExportViewModel : DatabaseUtilityViewModelBase
     {
+        public DataExportViewModel(IDatabaseConnectionStringRepository repository) :
+                    base(repository)
+        {
+
+        }
+
+        protected override void OnInitialize()
+        {
+            _Query = new ViewModelField<string>();
+            _GeneratedQuery = new ViewModelField<string>();
+            _GenerateIdentityInsert = new ViewModelField<bool>();
+            _ExportTableName = new ViewModelField<string>();
+
+            _Query.IsValid = true;
+            _GeneratedQuery.IsValid = true;
+            _GenerateIdentityInsert.IsValid = true;
+            _ExportTableName.IsValid = true;
+        }
+
+        private const string QueryPropertyName = "Query";
+
+        private ViewModelField<string> _Query;
+        public ViewModelField<string> Query
+        {
+            get
+            {
+                return _Query;
+            }
+            set
+            {
+                _Query = value;
+                RaisePropertyChanged(QueryPropertyName);
+            }
+        }
+
+        private const string ExportTableNamePropertyName = "ExportTableName";
+
+        private ViewModelField<string> _ExportTableName;
+        public ViewModelField<string> ExportTableName
+        {
+            get
+            {
+                return _ExportTableName;
+            }
+            set
+            {
+                _ExportTableName = value;
+                RaisePropertyChanged(ExportTableNamePropertyName);
+            }
+        }
+
+        private const string GenerateIdentityInsertPropertyName = "GenerateIdentityInsert";
+
+        private ViewModelField<bool> _GenerateIdentityInsert;
+        public ViewModelField<bool> GenerateIdentityInsert
+        {
+            get
+            {
+                return _GenerateIdentityInsert;
+            }
+            set
+            {
+                _GenerateIdentityInsert = value;
+                RaisePropertyChanged(GenerateIdentityInsertPropertyName);
+            }
+        }
+
+        private const string GeneratedQueryPropertyName = "GeneratedQuery";
+
+        private ViewModelField<string> _GeneratedQuery;
+        public ViewModelField<string> GeneratedQuery
+        {
+            get
+            {
+                return _GeneratedQuery;
+            }
+            set
+            {
+                _GeneratedQuery = value;
+                RaisePropertyChanged(GeneratedQueryPropertyName);
+            }
+        }
+
+        private ICommand _RunQueryCommand;
+        public ICommand RunQueryCommand
+        {
+            get
+            {
+                if (_RunQueryCommand == null)
+                {
+                    _RunQueryCommand = new RelayCommand(RunQuery);
+                }
+
+                return _RunQueryCommand;
+            }
+        }
+        private void RunQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        private ICommand _CreateInsertScriptCommand;
+        public ICommand CreateInsertScriptCommand
+        {
+            get
+            {
+                if (_CreateInsertScriptCommand == null)
+                {
+                    _CreateInsertScriptCommand = new RelayCommand(CreateInsertScript);
+                }
+
+                return _CreateInsertScriptCommand;
+            }
+        }
+        private void CreateInsertScript()
+        {
+            throw new NotImplementedException();
+        }
+
+        private ICommand _CreateMergeIntoScriptCommand;
+        public ICommand CreateMergeIntoScriptCommand
+        {
+            get
+            {
+                if (_CreateMergeIntoScriptCommand == null)
+                {
+                    _CreateMergeIntoScriptCommand = new RelayCommand(CreateMergeIntoScript);
+                }
+
+                return _CreateMergeIntoScriptCommand;
+            }
+        }
+        private void CreateMergeIntoScript()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Benday.SqlUtils/test/Benday.SqlUtils.UnitTests/ViewModels/DataExportViewModelFixture.cs
@@ -0,0 +1,66 @@
+﻿using Benday.SqlUtils.Api.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Benday.SqlUtils.UnitTests.ViewModels
+{
+    [TestClass]
+    public class DataExportViewModelFixture : ViewModelFixtureBase
+    {
+        [TestInitialize]
+        public void OnTestInitialize()
+        {
+            _SystemUnderTest = null;
+            _DatabaseConnectionStringRepositoryInstance = null;
+        }
+
+        private DataExportViewModel _SystemUnderTest;
+        public DataExportViewModel SystemUnderTest
+        {
+            get
+            {
+                if (_SystemUnderTest == null)
+                {
+                    _SystemUnderTest = new DataExportViewModel(
+                        DatabaseConnectionStringRepositoryInstance);
+                }
+
+                return _SystemUnderTest;
+            }
+        }
+
+        private MockDatabaseConnectionStringRepository _DatabaseConnectionStringRepositoryInstance;
+        public MockDatabaseConnectionStringRepository DatabaseConnectionStringRepositoryInstance
+        {
+            get
+            {
+                if (_DatabaseConnectionStringRepositoryInstance == null)
+                {
+                    _DatabaseConnectionStringRepositoryInstance =
+                        new MockDatabaseConnectionStringRepository();
+
+                    _DatabaseConnectionStringRepositoryInstance.Add(
+                        Guid.NewGuid().ToString(), "connection 1",
+                        _ValidConnectionStringUseIntegratedSecurity);
+
+                    _DatabaseConnectionStringRepositoryInstance.Add(
+                        Guid.NewGuid().ToString(), "connection 2",
+                        _ValidConnectionStringUserNamePassword);
+                }
+
+                return _DatabaseConnectionStringRepositoryInstance;
+            }
+        }
+
+        [TestMethod]
+        public void WhenInitializedThenFieldPropertiesAreNotNull()
+        {
+            Assert.IsNotNull(SystemUnderTest.Query, "Query is null.");
+            Assert.IsNotNull(SystemUnderTest.GeneratedQuery, "GeneratedQuery is null.");
+            Assert.IsNotNull(SystemUnderTest.GenerateIdentityInsert, "GenerateIdentityInsert was null.");
+            Assert.IsNotNull(SystemUnderTest.ExportTableName, "ExportTableName was null.");
+        }
+    }
+}
