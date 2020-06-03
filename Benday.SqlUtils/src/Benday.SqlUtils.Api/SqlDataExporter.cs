95c84b418a1b0e181e917ded6b4ab97cf10b456d
Benjamin Day <benday@benday.com>
2020-06-03 15:21:27 -0400

    Moved merge into script generation.

--- Benday.SqlUtils/src/Benday.SqlUtils.Api/SqlDataExporter.cs
+++ Benday.SqlUtils/src/Benday.SqlUtils.Api/SqlDataExporter.cs
@@ -230,5 +230,214 @@ namespace Benday.SqlUtils.Api
 
             return builder.ToString();
         }
+
+        public string GetMergeIntoScript()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            var tableName = ExportTableName;
+            DataTable resultsToExportDataTable = QueryResults;
+
+            var primaryKeyColumnName = TableDescription.PrimaryKeyColumnName;
+
+            builder.AppendLine($"SET IDENTITY_INSERT {tableName} ON");
+            builder.AppendLine("GO");
+            builder.AppendLine();
+            builder.AppendLine($"MERGE INTO {tableName} AS Target");
+            builder.AppendLine($"USING(");
+            builder.AppendLine($"    VALUES");
+
+            var lastResultRow = resultsToExportDataTable.Rows[resultsToExportDataTable.Rows.Count - 1];
+
+            bool needsComma = false;
+
+            foreach (DataRow resultRow in resultsToExportDataTable.Rows)
+            {
+                var values = new CodeBuilder();
+
+                values.IncreaseIndent();
+
+                values.AppendLine("(");
+
+                values.IncreaseIndent();
+
+                needsComma = false;
+
+                foreach (DataColumn resultColumn in resultsToExportDataTable.Columns)
+                {
+                    if (resultColumn.DataType == typeof(byte[]))
+                    {
+                        // skip byte arrays & concurrency columns
+                        continue;
+                    }
+
+                    if (needsComma == true)
+                    {
+                        values.Append(", ");
+                        values.NewLine();
+                    }
+
+                    needsComma = true;
+
+                    if (resultRow[resultColumn.ColumnName] == DBNull.Value)
+                    {
+                        values.Append("NULL");
+                    }
+                    else if (resultColumn.DataType == typeof(bool))
+                    {
+                        bool valueAsBool = (bool)resultRow[resultColumn.ColumnName];
+
+                        if (valueAsBool == true)
+                        {
+                            values.Append("1");
+                        }
+                        else
+                        {
+                            values.Append("0");
+                        }
+                    }
+                    else if (resultColumn.DataType == typeof(string) ||
+                        resultColumn.DataType == typeof(Guid) ||
+                        resultColumn.DataType == typeof(DateTime))
+                    {
+                        values.Append("'");
+                        values.Append(resultRow[resultColumn.ColumnName].ToString().Replace("'", "''"));
+                        values.Append("'");
+                    }
+                    else
+                    {
+                        values.Append(resultRow[resultColumn.ColumnName].ToString());
+                    }
+                }
+
+                values.NewLine();
+
+                values.DecreaseIndent();
+
+                if (resultRow != lastResultRow)
+                {
+                    values.AppendLine("),");
+                }
+                else
+                {
+                    values.AppendLine(")");
+                }
+
+                values.DecreaseIndent();
+
+                builder.Append(values.ToString());
+            }
+
+            builder.AppendLine(")");
+
+            builder.AppendLine("AS Source (");
+
+            needsComma = false;
+
+            builder.Append("\t");
+
+            foreach (DataColumn resultColumn in resultsToExportDataTable.Columns)
+            {
+                if (resultColumn.DataType == typeof(byte[]))
+                {
+                    // skip byte arrays & concurrency columns
+                    continue;
+                }
+
+                if (needsComma == true)
+                {
+                    builder.Append(", ");
+                }
+
+                needsComma = true;
+                builder.Append(resultColumn.ColumnName);
+            }
+
+            builder.AppendLine();
+            builder.AppendLine(")");
+
+            builder.AppendLine($"ON Target.{primaryKeyColumnName} = Source.{primaryKeyColumnName}");
+            builder.AppendLine("WHEN MATCHED THEN UPDATE SET");
+
+            needsComma = false;
+
+            foreach (DataColumn resultColumn in resultsToExportDataTable.Columns)
+            {
+                if (resultColumn.DataType == typeof(byte[]))
+                {
+                    // skip byte arrays & concurrency columns
+                    continue;
+                }
+                else if (resultColumn.ColumnName == primaryKeyColumnName)
+                {
+                    // don't try to update the value for the primary key
+                    continue;
+                }
+
+                if (needsComma == true)
+                {
+                    builder.AppendLine(", ");
+                }
+
+                needsComma = true;
+                builder.Append("\t");
+                builder.Append(resultColumn.ColumnName);
+                builder.Append(" = Source.");
+                builder.Append(resultColumn.ColumnName);
+            }
+
+            builder.AppendLine();
+
+            builder.AppendLine("WHEN NOT MATCHED BY TARGET THEN");
+
+            needsComma = false;
+            builder.Append("\t");
+            builder.Append("INSERT (");
+            foreach (DataColumn resultColumn in resultsToExportDataTable.Columns)
+            {
+                if (resultColumn.DataType == typeof(byte[]))
+                {
+                    // skip byte arrays & concurrency columns
+                    continue;
+                }
+
+                if (needsComma == true)
+                {
+                    builder.Append(", ");
+                }
+
+                needsComma = true;
+                builder.Append(resultColumn.ColumnName);
+            }
+
+            builder.AppendLine(")");
+
+            needsComma = false;
+            builder.Append("\t");
+            builder.Append("VALUES (");
+            foreach (DataColumn resultColumn in resultsToExportDataTable.Columns)
+            {
+                if (resultColumn.DataType == typeof(byte[]))
+                {
+                    // skip byte arrays & concurrency columns
+                    continue;
+                }
+
+                if (needsComma == true)
+                {
+                    builder.Append(", ");
+                }
+
+                needsComma = true;
+                builder.Append(resultColumn.ColumnName);
+            }
+
+            builder.AppendLine(")");
+
+            builder.AppendLine("WHEN NOT MATCHED BY SOURCE THEN DELETE;");
+            builder.AppendLine("GO");
+
+            return builder.ToString();
+        }
     }
 }
