d743688f9f3c5300dcc7caca1615098a796b66bd
Benjamin Day <benday@benday.com>
2020-06-08 14:36:03 -0400

    Refactoring back to ObservableColletion.

---
+++ Benday.SqlUtils/src/Benday.SqlUtils.Presentation/ViewModels/SearchByNameResultRow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace Benday.SqlUtils.Presentation.ViewModels
+{
+    public class SearchByNameResultRow
+    {
+        public SearchByNameResultRow(DataRow fromValue)
+        {
+            Schema = fromValue["table_schema"].ToString();
+            Table = fromValue["table_name"].ToString();
+            Column = fromValue["column_name"].ToString();
+            DataType = fromValue["data_type"].ToString();
+            FieldLength = fromValue["character_maximum_length"].ToString();
+        }
+
+        public string Schema { get; set; }
+        public string Table { get; set; }
+        public string Column { get; set; }
+        public string DataType { get; set; }
+        public string FieldLength { get; set; }
+    }
+}
--- Benday.SqlUtils/src/Benday.SqlUtils.Presentation/ViewModels/SearchByTextColumnContentQueryViewModel.cs
+++ Benday.SqlUtils/src/Benday.SqlUtils.Presentation/ViewModels/SearchByTextColumnContentQueryViewModel.cs
@@ -411,25 +411,6 @@ order by c.table_name, c.column_name
         }
         */
 
-        public class TextQueryResultRow
-        {            
-            public TextQueryResultRow(DataRow fromValue, string query, int recordCount)
-            {
-                Schema = fromValue["table_schema"].ToString();
-                Table = fromValue["table_name"].ToString();
-                Column = fromValue["column_name"].ToString();
-                Records = recordCount;
-                Query = query;
-            }
-
-            public string Schema { get; set; }
-            public string Table { get; set; }
-            public string Column { get; set; }
-            public int Records { get; set; }
-            public string Query { get; set; }
-        }
-
-
         public DataTable ExecuteCommand(SqlCommand command)
         {
             DataSet results = new DataSet();
