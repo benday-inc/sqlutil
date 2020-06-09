b480034f88c1fbb7747fc90953eaf4986dea936e
Benjamin Day <benday@benday.com>
2020-06-09 10:24:57 -0400

    Added basic info to telemetry events.

--- Benday.SqlUtils/src/Benday.SqlUtils.WpfUi/App.xaml.cs
+++ Benday.SqlUtils/src/Benday.SqlUtils.WpfUi/App.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,7 +29,7 @@ namespace Benday.SqlUtils.WpfUi
                 return _LocatorInstance;
             }
         }
-        
+
 
         private void Application_Exit(object sender, ExitEventArgs e)
         {
@@ -38,7 +39,24 @@ namespace Benday.SqlUtils.WpfUi
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            LocatorInstance.TelemetryClient.TrackEvent("Startup");
+            var client = LocatorInstance.TelemetryClient;
+            
+            if (String.IsNullOrWhiteSpace(SqlUtilSettings.Default.AppInsightsUserId) == true)
+            {
+                SqlUtilSettings.Default.AppInsightsUserId = Guid.NewGuid().ToString();
+                SqlUtilSettings.Default.Save();
+            }
+
+            client.Context.User.Id = SqlUtilSettings.Default.AppInsightsUserId;
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            client.Context.GlobalProperties.Add("OSVersion", Environment.OSVersion.ToString());
+            client.Context.GlobalProperties.Add("AppName", System.AppDomain.CurrentDomain.FriendlyName);
+            client.Context.GlobalProperties.Add("AppVersion", assembly.FullName);
+            client.Context.GlobalProperties.Add("CurrentCulture", CultureInfo.CurrentCulture.ToString());
+
+            client.TrackEvent("Startup");
         }
 
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
